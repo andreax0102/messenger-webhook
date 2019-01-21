01dc3eca60cc4effeaf6db84800cbe424475645d
andreaximena <andreaximena@fb.com>
2019-01-20 22:01:07 -0800

    remove displaying body

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ app.listen(process.env.PORT || 1337, () => console.log('webhook is listening'));
 app.post('/webhook', (req, res) => {
 
   let body = req.body;
-  console.log("body: ", body);
+  // console.log("body: ", body);
   // Checks this is an event from a page subscription
   if (body.object === 'page') {
 
